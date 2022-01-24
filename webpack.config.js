06a9f11e6fcf1e47a1b935ca802b7ac3afe42320
xandrf <xandrf@protonmail.com>
2022-01-24 09:58:10 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode:"production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    static: "./pub",
+    port: 9000
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'pub'),
+  },
+};
