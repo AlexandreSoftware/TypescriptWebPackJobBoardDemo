cef14e8f797f103b12b7dffacf5a5f86b93b925d
xandrf <xandrf@protonmail.com>
2022-01-24 22:40:27 -0300

    added sidebar

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,17 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      }
     ],
   },
   devServer: {
@@ -17,10 +28,12 @@ module.exports = {
     port: 9000
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js','css','sass','scss'],
   },
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'pub'),
   },
+  plugins:[
+  ]
 };
