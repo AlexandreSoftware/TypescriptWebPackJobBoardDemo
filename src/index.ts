8bda723f0e5abe91bbd83c7cb0a0efc08b6c1570
xandrf <xandrf@protonmail.com>
2022-01-26 14:19:29 -0300

    finished footer

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
 import "./index.scss"
+import * as $ from 'jquery';
+import axios from "axios";
+//     console.log()
 console.log()
+$("[link-to]").each((iterator,i)=>{
+
+    $(i).on("click",(y)=>{
+        let link = $(y.target).attr("link-to");
+        setcontent(link)
+    })
+}
+)
+function setcontent(link:string){
+    axios(link).then(z=>{
+        $("#content").html(k=>z.data);
+    });
+}
+setcontent($("[link-to]#home").attr("link-to"))
